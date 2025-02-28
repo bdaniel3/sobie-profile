630c684ba18a489c41dce83d714f3c7362c41cf7
Brittney <149526365+bdaniel3@users.noreply.github.com>
2025-02-27 18:54:29 -0600

    Add files via upload

---
+++ public/scripts/script.js
@@ -0,0 +1,8 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+  
+  //more code here...
+});
+
+console.log("This is Brittney Daniel's Web Profile");
